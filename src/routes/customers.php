277f198e10712644e41052e67fc3d4817b54bc1b
Alex <awarrington0895@gmail.com>
2017-11-25 20:45:11 -0600

    Update displays proper functionality
    Successfully updated the sample patient.

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -115,7 +115,7 @@ $app->post('/api/patient/add', function(Request $request, Response $response) {
 
             $stmt->execute();
 
-            echo '{"notice": {"text": "Customer Added"}';
+            echo '{"notice": {"text": "Patient Added"}';
     
         } catch(PDOException $e) {
             echo '{"error": {"text": '.$e->getMessage().'}';
@@ -124,26 +124,38 @@ $app->post('/api/patient/add', function(Request $request, Response $response) {
     });
 
 // Update Customer
-$app->put('/api/customer/update/{id}', function(Request $request, Response $response) {
+$app->put('/api/patient/update/{id}', function(Request $request, Response $response) {
 
-        $id = $request->getAttribute('id');
-        $first_name = $request->getParam('first_name');
-        $last_name = $request->getParam('last_name');
-        $phone = $request->getParam('phone');
-        $email = $request->getParam('email');
-        $address = $request->getParam('address');
-        $city = $request->getParam('city');
-        $state = $request->getParam('state');
+        $id =               $request->getAttribute('id');
+        $first_name =       $request->getParam('first_name');
+        $last_name =        $request->getParam('last_name');
+        $preferred_name =   $request->getParam('preferred_name');
+        $address =          $request->getParam('address');
+        $city =             $request->getParam('city');
+        $state =            $request->getParam('state');
+        $zip =              $request->getParam('zip');
+        $prim_phone =       $request->getParam('prim_phone');
+        $sec_phone =        $request->getParam('sec_phone');
+        $email =            $request->getParam('email');
+        $allergies =        $request->getParam('allergies');
+        $ins_name =         $request->getParam('ins_name');
+        $ins_number =       $request->getParam('ins_number');
     
-        $sql = "UPDATE customers SET
-                    first_name  = :first_name,
-                    last_name   = :last_name,
-                    phone       = :phone,
-                    email       = :email,
-                    address     = :address,
-                    city        = :city,
-                    state       = :state
-                WHERE id = {$id}";
+        $sql = "UPDATE patient SET
+                    FirstName     = :first_name,
+                    LastName      = :last_name,
+                    PreferredName = :preferred_name,
+                    Address       = :address,
+                    City          = :city,
+                    State         = :state,
+                    Zip           = :zip,
+                    PrimPhone     = :prim_phone,
+                    SecPhone      = :sec_phone,
+                    Email         = :email,
+                    Allergies     = :allergies,
+                    InsName       = :ins_name,
+                    InsNumber     = :ins_number
+                WHERE PtNum = {$id}";
         
         try {
             // Get DB Object
@@ -153,18 +165,24 @@ $app->put('/api/customer/update/{id}', function(Request $request, Response $resp
             $db = $db->connect();
     
             $stmt = $db->prepare($sql);
-            
-            $stmt->bindParam(':first_name', $first_name);
-            $stmt->bindParam(':last_name',  $last_name);
-            $stmt->bindParam(':phone',      $phone);
-            $stmt->bindParam(':email',      $email);
-            $stmt->bindParam(':address',    $address);
-            $stmt->bindParam(':city',       $city);
-            $stmt->bindParam(':state',      $state);
+
+            $stmt->bindParam(':first_name',      $first_name);
+            $stmt->bindParam(':last_name',       $last_name);
+            $stmt->bindParam(':preferred_name',  $preferred_name);
+            $stmt->bindParam(':address',         $address);
+            $stmt->bindParam(':city',            $city);
+            $stmt->bindParam(':state',           $state);
+            $stmt->bindParam(':zip',             $zip);
+            $stmt->bindParam(':prim_phone',      $prim_phone);
+            $stmt->bindParam(':sec_phone',       $sec_phone);
+            $stmt->bindParam(':email',           $email);
+            $stmt->bindParam(':allergies',       $allergies);
+            $stmt->bindParam(':ins_name',        $ins_name);
+            $stmt->bindParam(':ins_number',      $ins_number);
 
             $stmt->execute();
 
-            echo '{"notice": {"text": "Customer Updated"}';
+            echo '{"notice": {"text": "Patient Updated"}';
     
         } catch(PDOException $e) {
             echo '{"error": {"text": '.$e->getMessage().'}';
